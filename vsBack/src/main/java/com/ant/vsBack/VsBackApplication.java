73f98b426cc005f7769e989dd172a7a9226200be
“mohamed511 <mohamedsayed194@hotmail.com>
2021-10-14 21:16:31 +0200

    first Commit

---
+++ vsBack/src/main/java/com/ant/vsBack/VsBackApplication.java
@@ -0,0 +1,13 @@
+package com.ant.vsBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsBackApplication.class, args);
+	}
+
+}
---
+++ vsBack/src/test/java/com/ant/vsBack/VsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ant.vsBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
