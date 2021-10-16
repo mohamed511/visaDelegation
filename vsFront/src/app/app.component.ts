9d8bd23267fac33b3b4ceaf3f7ddc7752d88c35d
“mohamed511 <mohamedsayed194@hotmail.com>
2021-10-16 21:47:37 +0200

    the Layout

--- vsFront/src/app/app.component.ts
+++ vsFront/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vsFront';
+
+  opened = false;
+
+  toggleSidebar() {
+    this.opened = !this.opened;
+  }
 }
--- vsFront/src/app/app.module.ts
+++ vsFront/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SidebarModule } from 'ng-sidebar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainTemplateComponent } from './layout/main-template/main-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
